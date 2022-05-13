a62062ab827f8fceed79578caaecfbf5eb9a6f44
tsduc <tsduc0996@gmail.com>
2022-05-13 11:07:09 +0700

    Add project files.

---
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapMvcAttributeRoutes();
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Trang1",
+                url: "trang",
+                defaults: new { controller = "New", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebApplication1.Controllers" }
+            );
+
+            routes.MapRoute(
+                name: "Trang2",
+                url: "trang_2",
+                defaults: new { controller = "New", action = "Index2" },
+                namespaces: new[] { "WebApplication1.Controllers" }
+            );
+        }
+    }
+}
